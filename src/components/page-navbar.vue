871a1bca4eda58bdef4ef0d5b2b94758200b5a55
adammacias <eu@adammacias.com>
2016-10-21 00:32:56 -0200

    Update page-navbar component

--- src/components/page-navbar.vue
+++ src/components/page-navbar.vue
@@ -1,6 +1,25 @@
 <template>
-  <div>
-    <p>Você esta vendo o componente <code>{{ page.title }}</code></p>
+  <div id="page-navbar">
+    <nav class="navbar navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" v-on:click="toggleNavbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Vue School</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse" :class="{ in: isOpenNavbar }">
+          <ul class="nav navbar-nav">
+            <li><a href="#">Principal</a></li>
+            <li><a href="#">Disciplinas</a></li>
+            <li><a href="#">Alunos</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
 
@@ -11,11 +30,20 @@ export default {
 
   data () {
     return {
-      page: { title: "Page Navbar" }
+    	isOpenNavbar: false
     };
+  },
+
+  methods: {
+  	toggleNavbar () {
+  		this.isOpenNavbar = !this.isOpenNavbar
+  	}
   }
 };
 </script>
 
-<style lang="css" scoped>
+<style scoped>
+  .navbar {
+    border-radius: 0!important;
+  }
 </style>
