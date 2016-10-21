94666fb5bd02dfcc39b62c25d46ac8b92536d0af
adammacias <eu@adammacias.com>
2016-10-21 00:29:37 -0200

    Implements components created in App component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <page-navbar></page-navbar>
+    <div class="container">
+      <page-main></page-main>
+      <disciplina-main></disciplina-main>
+      <disciplina-add></disciplina-add>
+      <disciplina-single></disciplina-single>
+      <aluno-main></aluno-main>
+      <aluno-add></aluno-add>
+      <aluno-single></aluno-single>
+    </div>
+    <page-footer></page-footer>
   </div>
 </template>
 
 <script>
+import PageNavbar from './components/page-navbar.vue'
+import PageFooter from './components/page-footer.vue'
+import PageMain from './components/page-main.vue'
+import DisciplinaMain from './components/disciplina-main.vue'
+import DisciplinaAdd from './components/disciplina-add.vue'
+import DisciplinaSingle from './components/disciplina-single.vue'
+import AlunoMain from './components/aluno-main.vue'
+import AlunoAdd from './components/aluno-add.vue'
+import AlunoSingle from './components/aluno-single.vue'
+
 export default {
   name: 'app',
+  components: {
+    PageNavbar, PageFooter, PageMain, 
+    DisciplinaMain, DisciplinaAdd, DisciplinaSingle,
+    AlunoMain, AlunoAdd, AlunoSingle
+
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
